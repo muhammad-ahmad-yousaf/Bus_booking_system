de60f3711a7787d95100ab0b33bf247710eb00b6
muhammad-ahmad-yousaf <ahmadjutt611@gmail.com>
2025-08-21 13:10:56 +0500

    update design

--- Gemfile
+++ Gemfile
@@ -66,3 +66,5 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
+
+gem "rubocop-erb", "~> 0.6.0", require: false
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :bookings
+  validates :name, presence: true
 
   enum :role, [:user,:admin]
 end
