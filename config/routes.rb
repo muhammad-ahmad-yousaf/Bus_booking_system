017b0bcab7266e077ec40bdf6cefcdad1da8bb34
muhammad-ahmad-yousaf <ahmadjutt611@gmail.com>
2025-08-27 18:24:23 +0500

    rubocop fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 require "sidekiq/web"
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: "users/registrations" ,sessions: "users/sessions"}
+  devise_for :users, controllers: { registrations: "users/registrations", sessions: "users/sessions" }
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
